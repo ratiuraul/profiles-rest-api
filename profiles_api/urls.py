34b43be421ad1b5194f7f2797d2ea75ac340f70b
Raul <ratiuraul@github.com>
2024-10-02 09:53:57 +0300

    Added profiles viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,6 +10,8 @@ router = DefaultRouter()
 router.register('hellow-viewset', views.HelloViewSet,
                 base_name='hello-viewset')
 
+router.register('profile', views.UserProfileViewSet)
+
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('', include(router.urls))
