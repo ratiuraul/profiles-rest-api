72a1c4b56874162dea6441bccb899c5613ae915e
Raul <ratiuraul@github.com>
2024-10-02 10:01:23 +0300

    add filtering to profile api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from profiles_api import permissions
+from rest_framework import filters
 
+from profiles_api import permissions
 from profiles_api import serializers
 from profiles_api import models
 
@@ -104,3 +105,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
